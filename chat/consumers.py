5f6b94468595940abe314f95a5533d20bc1ca730
SUMIT-STHA195 <sumitshrestha2313@gmail.com>
2026-01-01 14:47:03 +0545

    Notification realtime update and unseen notification fetch added

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,26 +1,9 @@
-# import json
-
-# from channels.generic.websocket import WebsocketConsumer
-
-# class ChatConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.accept()
-
-#     def disconnect(self, close_code):
-#         pass
-
-#     def receive(self, text_data):
-#         text_data_json=json.loads(text_data)
-#         message=text_data_json["message"]
-#         self.send(text_data=json.dumps({'message':message}))
-
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import Message, Room
+from .models import Message, Room, Notification
 from django.contrib.auth.models import User
 from channels.db import database_sync_to_async
 from zoneinfo import ZoneInfo
-
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -45,25 +28,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
         user = self.scope['user'].username
         print(user)
         room_name = self.room_name
-        message=await self.save_message(message, room_name, user)
+        message = await self.save_message(message, room_name, user)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
-                "message":message
-                
+                "message": message
+
             }
         )
 
     async def chat_message(self, event):
-        context=event['message']
-        # print(context.sender.username)
-        # print("CHAT MESSAGE CALLED:", message)
-        # print(user)
-        # print(user)
+        context = event['message']
         await self.send(text_data=json.dumps({"sender": context.sender.username,
-                "content": context.content,
-                "timestamp":context.timestamp.astimezone(ZoneInfo('Asia/Kathmandu')).strftime("%H:%M")}))
+                                              "content": context.content,
+                                              "timestamp": context.timestamp.astimezone(ZoneInfo('Asia/Kathmandu')).strftime("%H:%M")}))
 
     @database_sync_to_async
     def save_message(self, message, room_name, user):
@@ -76,4 +55,64 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
         message.save()
         return message
+
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        if self.user.is_authenticated:
+            self.group_name = f'notify_{self.user.id}'
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
+            notification_context = await self.get_notification()
+            await self.send(text_data=json.dumps({
+                "type": "initial_notification",
+                "notification_context": notification_context,
+            }))
+        else:
+            await self.close()
+
+    async def disconnect(self, code):
+        if self.scope['user'].is_authenticated:
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name
+            )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        if data.get('action') == 'mark_read':
+            await self.mark_all_as_read()
+
+    @database_sync_to_async
+    def mark_all_as_read(self):
+        Notification.objects.filter(
+            recipient=self.user, is_read=False).update(is_read=True)
     
+    async def send_notification(self, event):
+        await self.send(text_data=json.dumps({
+            'type':'send_notification',
+            'notification_context': event['notification_context'],
+        }))
+
+    @database_sync_to_async
+    def get_notification(self):
+        # notification = Notification.objects.filter(recipient_id=self.user.id)
+        notifications = Notification.objects.filter(
+            recipient=self.user,
+            is_read=False
+        ).select_related('message__sender', 'message__room')
+        notification_context = [
+            {
+                "message": f"New message from {x.message.sender.first_name}",
+                "sender_username": x.message.sender.username,
+                "is_group": x.message.room.is_group,
+                "group_name": x.message.room.room_name,
+                "is_read": x.is_read,
+            }
+            for x in notifications
+        ]
+        return notification_context
--- chat/signals.py
+++ chat/signals.py
@@ -1,24 +1,41 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message, Notification
-from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwarg):
     if created:
-        if instance.room.is_group:
-            receiver_user=instance.room.members.exclude(id=instance.sender.id)
-            save_notification(instance=instance,recipient=receiver_user)
-        else:
-            receiver_user = instance.room.members.exclude(
-                id=instance.sender.id)
-            receiver_user=instance.room.members.exclude(id=instance.sender.id)
-            save_notification(instance=instance,recipient=receiver_user)
+        receiver_user = instance.room.members.exclude(
+            id=instance.sender.id)
+        receiver_user = instance.room.members.exclude(id=instance.sender.id)
+        save_notification(instance=instance, recipient=receiver_user)
 
-def save_notification(instance,recipient):
-    notification=[Notification(
-                message=instance,
-                recipient=user
-            ) for user in recipient]
+
+def save_notification(instance, recipient):
+    notification = [Notification(
+        message=instance,
+        recipient=user
+    ) for user in recipient]
     Notification.objects.bulk_create(notification)
+    channel_layer = get_channel_layer()
+    for user in recipient:
+        # print(x.id)
+        # TODO :instead of direct sending fetch from db and send notification, only send unseen message to the group or user and
+        group_name = f"notify_{user.id}"
+        notification_context = [{
+            "message": f"New message from {instance.sender.first_name}",
+            "sender_username": instance.sender.username,
+            "is_group": instance.room.is_group,
+            "group_name": instance.room.room_name,
+            "is_read": False,
+        }]
+        async_to_sync(channel_layer.group_send)(
+            group_name,
+            {
+                "type": "send_notification",
+                "notification_context": notification_context,
+            }
+        )
